ac1324ff1034424ae8f357a97734380c7bbc874d
mihaimaganu17 <m3m0ry@elonmusk.local>
2021-02-09 21:42:22 +0100

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let f = fs::read("/Users/m3m0ry/fun/apfel/samples/hello_world");
+    let contents = match f {
+        Ok(bytes) => bytes,
+        Err(e) => Vec::new(),
+    };
+
+    for byte in contents.iter() {
+        print!("{:#02x} ",byte);
+    }
 }
